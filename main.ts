ae76e7ed959b5a6dbe772060b0ecbb44f862f98d
shaoziyang <shaoziyang@126.com>
2018-04-07 23:43:08 +0800

    modify block define

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@
 /**
  * DS1307 block
  */
-//% weight=100 color=#8000f0 icon="\uf017"
+//% weight=100 color=#8000f0 icon="\uf017" block="DS1307"
 namespace DS1307 {
     let DS1307_I2C_ADDR = 104;
     let DS1307_REG_SECOND = 0
@@ -55,7 +55,8 @@ namespace DS1307 {
     /**
      * start ds1307 (go on)
      */
-    //% block
+    //% blockId="DS1307_START" block="start"
+    //% weight=52 blockGap=8 
     export function start() {
         let t = getSecond()
         setSecond(t & 0x7f)
@@ -64,7 +65,8 @@ namespace DS1307 {
     /**
      * stop ds1307 (pause)
      */
-    //% block
+    //% blockId="DS1307_STOP" block="pause"
+    //% weight=51 blockGap=8 
     export function stop() {
         let t = getSecond()
         setSecond(t | 0x80)
@@ -73,7 +75,8 @@ namespace DS1307 {
     /**
      * get Year
      */
-    //% block
+    //% blockId="DS1307_GET_YEAR" block="year"
+    //% weight=99 blockGap=8 
     export function getYear(): number {
         return (HexToDec(getReg(DS1307_REG_YEAR)) + 2000)
     }
@@ -82,7 +85,8 @@ namespace DS1307 {
      * set year
      * @param dat is the Year will be set, eg: 2018
      */
-    //% block
+    //% blockId="DS1307_SET_YEAR" block="set year %dat"
+    //% weight=69 blockGap=8 
     export function setYear(dat: number): void {
         setReg(DS1307_REG_YEAR, DecToHex(dat % 100))
     }
@@ -90,7 +94,8 @@ namespace DS1307 {
     /**
      * get Month
      */
-    //% block
+    //% blockId="DS1307_GET_MONTH" block="month"
+    //% weight=98 blockGap=8 
     export function getMonth(): number {
         return HexToDec(getReg(DS1307_REG_MONTH))
     }
@@ -99,7 +104,8 @@ namespace DS1307 {
      * set month
      * @param dat is Month will be set.  eg: 2
      */
-    //% block
+    //% blockId="DS1307_SET_MONTH" block="set month %dat"
+    //% weight=68 blockGap=8
     //% dat.min=1 dat.max=12
     export function setMonth(dat: number): void {
         setReg(DS1307_REG_MONTH, DecToHex(dat % 13))
@@ -108,7 +114,8 @@ namespace DS1307 {
     /**
      * get Day
      */
-    //% block
+    //% blockId="DS1307_GET_DAY" block="day"
+    //% weight=97 blockGap=8 
     export function getDay(): number {
         return HexToDec(getReg(DS1307_REG_DAY))
     }
@@ -117,7 +124,8 @@ namespace DS1307 {
      * set day
      * @param dat is the Day will be set, eg: 15
      */
-    //% block
+    //% blockId="DS1307_SET_DAY" block="set day %dat"
+    //% weight=67 blockGap=8
     //% dat.min=1 dat.max=31
     export function setDay(dat: number): void {
         setReg(DS1307_REG_DAY, DecToHex(dat % 32))
@@ -126,7 +134,8 @@ namespace DS1307 {
     /**
      * get Week Day
      */
-    //% block
+    //% blockId="DS1307_GET_WEEKDAY" block="weekday"
+    //% weight=96 blockGap=8 
     export function getWeekday(): number {
         return HexToDec(getReg(DS1307_REG_WEEKDAY))
     }
@@ -135,7 +144,8 @@ namespace DS1307 {
      * set weekday
      * @param dat is the Week Day will be set, eg: 4
      */
-    //% block
+    //% blockId="DS1307_SET_WEEKDAY" block="set weekday %dat"
+    //% weight=66 blockGap=8
     //% dat.min=1 dat.max=7
     export function setWeekday(dat: number): void {
         setReg(DS1307_REG_WEEKDAY, DecToHex(dat % 8))
@@ -144,7 +154,8 @@ namespace DS1307 {
     /**
      * get Hour
      */
-    //% block
+    //% blockId="DS1307_GET_HOUR" block="hour"
+    //% weight=95 blockGap=8 
     export function getHour(): number {
         return HexToDec(getReg(DS1307_REG_HOUR))
     }
@@ -153,7 +164,8 @@ namespace DS1307 {
      * set hour
      * @param dat is the Hour will be set, eg: 0
      */
-    //% block
+    //% blockId="DS1307_SET_HOUR" block="set hour %dat"
+    //% weight=65 blockGap=8
     //% dat.min=0 dat.max=23
     export function setHour(dat: number): void {
         setReg(DS1307_REG_HOUR, DecToHex(dat % 24))
@@ -162,7 +174,8 @@ namespace DS1307 {
     /**
      * get Minute
      */
-    //% block
+    //% blockId="DS1307_GET_MINUTE" block="minute"
+    //% weight=94 blockGap=8 
     export function getMinute(): number {
         return HexToDec(getReg(DS1307_REG_MINUTE))
     }
@@ -171,7 +184,8 @@ namespace DS1307 {
      * set minute
      * @param dat is the Minute will be set, eg: 0
      */
-    //% block
+    //% blockId="DS1307_SET_MINUTE" block="set minute %dat"
+    //% weight=64 blockGap=8
     //% dat.min=0 dat.max=59
     export function setMinute(dat: number): void {
         setReg(DS1307_REG_MINUTE, DecToHex(dat % 60))
@@ -180,7 +194,8 @@ namespace DS1307 {
     /**
      * get Second
      */
-    //% block
+    //% blockId="DS1307_GET_SECOND" block="second"
+    //% weight=93 blockGap=8 
     export function getSecond(): number {
         return HexToDec(getReg(DS1307_REG_SECOND))
     }
@@ -189,7 +204,8 @@ namespace DS1307 {
      * set second
      * @param dat is the Second will be set, eg: 0
      */
-    //% block
+    //% blockId="DS1307_SET_SECOND" block="set second %dat"
+    //% weight=63 blockGap
     //% dat.min=0 dat.max=59
     export function setSecond(dat: number): void {
         setReg(DS1307_REG_SECOND, DecToHex(dat % 60))
@@ -205,7 +221,8 @@ namespace DS1307 {
      * @param minute is the Minute will be set, eg: 0
      * @param second is the Second will be set, eg: 0
      */
-    //% block
+    //% blockId="DS1307_SET_DATETIME" block="set year %year|month %month|day %day|weekday %weekday|hour %hour|minute %minute|second %second"
+    //% weight=60 blockGap
     export function DateTime(year: number, month: number, day: number, weekday: number, hour: number, minute: number, second: number): void {
         let buf = pins.createBuffer(8);
         buf[0] = DS1307_REG_SECOND;
